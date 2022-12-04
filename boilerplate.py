1b1302158e2d0859eefe6af1d888269f284f2097
Bastian Baumeister <bastian.baumeister@iteratec.com>
2022-12-04 12:32:08 +0100

    added boilerplate

---
+++ boilerplate.py
@@ -0,0 +1,21 @@
+import string
+
+file = "x/input.txt"
+#  file = "x/test.txt"
+
+def main():
+    print("=== Part One ===")
+    with open(file, "r", encoding="utf-8") as input_file:
+        line = input_file.readline().replace("\n", "")
+
+        while line != "":
+            pass
+
+            line = input_file.readline().replace("\n", "")
+        line = input_file.readline().replace("\n", "")
+
+    print(f"result {}")
+
+
+if __name__ == "__main__":
+    main()
